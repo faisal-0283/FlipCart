584ec630eb3c76ea7b4f5b9da5bf845063c07ccf
faisal-0283 <faisalahmed0283@gmail.com>
2025-07-19 23:09:21 +0600

    core portion is almost completed

---
+++ core/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def faq(request):
+    return render(request, 'core/faq.html')
+
+def terms(request):
+    return render(request, 'core/terms.html')
+
+
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home') 
+        else:
+            messages.error(request, 'Username or Password wrong')
+
+    return render(request, 'core/login.html')
+
+
+
